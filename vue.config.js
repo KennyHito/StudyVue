895c82dbe0ab6a63b46758465b4f114f32c66fc1
yuhaitao <yuhaitao1@lczq.com>
2025-03-13 14:14:28 +0800

    modify: 增加代码注释

--- vue.config.js
+++ vue.config.js
@@ -17,14 +17,14 @@ module.exports = defineConfig({
       // xs代表本地服务器学生接口
       '/xs': {
         target: 'http://localhost:5000',
-				pathRewrite:{'^/xs':''},
+				pathRewrite:{'^/xs':''},// 重写路径把xs替换成空
         // ws: true, //用于支持websocket(默认值true)
         // changeOrigin: true //用于控制请求头中的host值(默认值true)
       },
       //qc代表本地服务器汽车接口
       '/qc': {
         target: 'http://localhost:5001',
-				pathRewrite:{'^/qc':''},
+				pathRewrite:{'^/qc':''},//重写路径把qc替换成空
         // ws: true, //用于支持websocket(默认值true)
         // changeOrigin: true //用于控制请求头中的host值(默认值true)
       }
