bdbd217abe134a92f518c2a11e5b32648473b5f8
yuhaitao <yuhaitao1@lczq.com>
2025-03-17 11:04:11 +0800

    modify: 更新

--- src/config/httpService.js
+++ src/config/httpService.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //封装请求方法
 async function formatReq(type, url, data) {
-  return new Promise((reslove, reject,) => {
+  return new Promise((reslove, reject) => {
     axios({
       method: type,
       url: `${url}`,
