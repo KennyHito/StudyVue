025920348e72103181946885d6f0ffde93c0d9a7
yuhaitao <yuhaitao1@lczq.com>
2025-03-24 14:25:03 +0800

    modify: 调整路由

--- src/router/index.js
+++ src/router/index.js
@@ -9,125 +9,67 @@ const router = new Router({
       redirect: '/HomePage',
     },
     {
-      path: "/HomePage",//首页
-      name: "HomePage",
+      path: "/HomePage",
       component: () => import("../views/HomePage"),
-      meta: {
-        title: "首页"
-      },
     },
     {
       path: "/TestPage",
-      name: "TestPage",
       component: () => import("../views/TestPage/TestPage"),
-      meta: {
-        title: "TestPage"
-      },
     },
     {
       path: "/AddressPage",
-      name: "AddressPage",
       component: () => import("../views/AddressPage/AddressPage"),
-      meta: {
-        title: "地址列表"
-      },
     },
     {
       path: "/EditAddress",
-      name: "EditAddress",
       component: () => import("../views/AddressPage/EditAddress"),
-      meta: {
-        title: "编辑地址"
-      },
     },
     {
       path: "/TodoList",
-      name: "TodoList",
       component: () => import("../views/TodoList/TodoList"),
-      meta: {
-        title: "TodoList"
-      },
     },
     {
       path: "/LocalStorage",
-      name: "LocalStorage",
       component: () => import("../views/LocalStorage/LocalStorage"),
-      meta: {
-        title: "LocalStorage"
-      },
     },
     {
       path: "/SchoolOrStudent",
-      name: "SchoolOrStudent",
       component: () => import("../views/SchoolOrStudent/SchoolOrStudent"),
-      meta: {
-        title: "SchoolOrStudent"
-      },
     },
     {
       path: "/GlobalEvent",
-      name: "GlobalEvent",
       component: () => import("../views/GlobalEvent/GlobalEvent"),
-      meta: {
-        title: "GlobalEvent"
-      },
     },
     {
       path: "/PubSubPage",
-      name: "PubSubPage",
       component: () => import("../views/PubSubPage/PubSubPage"),
-      meta: {
-        title: "PubSubPage"
-      },
     },
     {
       path: "/AnimationPage",
-      name: "AnimationPage",
       component: () => import("../views/AnimationPage/AnimationPage"),
-      meta: {
-        title: "AnimationPage"
-      },
     },
     {
       path: "/SearchGithubPage",
-      name: "SearchGithubPage",
       component: () => import("../views/SearchGithubPage/SearchGithubPage"),
-      meta: {
-        title: "SearchGithubPage"
-      },
     },
     {
       path: "/SlotPage",
-      name: "SlotPage",
       component: () => import("../views/SlotPage/SlotPage"),
-      meta: {
-        title: "SlotPage"
-      },
     },
     {
       path: "/VuexPage",
-      name: "VuexPage",
       component: () => import("../views/VuexPage/VuexPage"),
-      meta: {
-        title: "VuexPage"
-      },
     },
     {
       path: "/VuexSharedPage",
-      name: "VuexSharedPage",
       component: () => import("../views/VuexSharedPage/VuexSharedPage"),
-      meta: {
-        title: "VuexSharedPage"
-      },
     },
   ]
 });
 
 router.beforeEach((to, from, next) => {
-  const title = to.meta && to.meta.title;
-  if (title) {
-    document.title = title;
-  }
+  console.log('⚠️测试打印的内容:--->from',from);
+  console.log('⚠️测试打印的内容:--->to',to);
   next();
 });
 
